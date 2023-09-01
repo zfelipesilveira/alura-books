fd7041091d0174225a4cb76c5355abcba7bf91a6
Zomer Silveira <zfelipesilveira@gmail.com>
2023-09-01 09:48:33 -0300

    melhorando o componente

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import './App.css';
-import logo from './imagens/logo.svg';
+import Logo from './componentes/logo/index.js'
 
 function App() {
   return (
     <div className="App">
       <header className='App-header'>
-        <div className='logo'>
-          <img src={logo} alt='logo'></img>
-          <p><strong>Alura</strong>Books</p>
-        </div>
+        <Logo></Logo>
       </header>
     </div>
   );
