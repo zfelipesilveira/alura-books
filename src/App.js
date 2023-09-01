6ba6df75c5d686b75a3fd830a11de3dc0f993e27
Zomer Silveira <zfelipesilveira@gmail.com>
2023-09-01 09:18:18 -0300

    terminando o primeiro componente

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import logo from './imagens/logo.svg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
+      <header className='App-header'>
+        <div className='logo'>
+          <img src={logo} alt='logo'></img>
+          <p><strong>Alura</strong>Books</p>
+        </div>
       </header>
     </div>
   );
