14a9a32db18f284e7920cebb77c918dc9fb7be95
Zomer Silveira <zfelipesilveira@gmail.com>
2023-09-06 14:20:48 -0300

    new jsx logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import Logo from './componentes/logo/index.js'
 
+const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE'];
+
+
 function App() {
   return (
     <div className="App">
       <header className='App-header'>
         <Logo></Logo>
+        <ul className='opcoes'>
+                    { textoOpcoes.map ( (texto) => (
+                        <li className='opcao'><p>{texto}</p></li>
+                    ) ) }
+        </ul>
       </header>
     </div>
   );
