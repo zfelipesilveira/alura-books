4b37d51300628196268ffd12e8dbe2d5ed8f3d86
Zomer Silveira <zfelipesilveira@gmail.com>
2023-10-18 09:43:56 -0300

    componentizando melhor

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
 import './App.css';
-import Logo from './componentes/logo/index.js'
-import perfil from './imagens/perfil.svg'
-import sacola from './imagens/sacola.svg'
-
-
-const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE'];
-const icones = [perfil, sacola];
-
+import Header from './componentes/Header'
 
 function App() {
-  return (
-    <div className="App">
-      <header className='App-header'>
-        <Logo></Logo>
-        <ul className='opcoes'>
-          { textoOpcoes.map ( (texto) => (
-            <li className='opcao'><p>{texto}</p></li>
-          ) ) }       
-        </ul>
-        <ul className='icones'>
-            { icones.map( (icone) => (
-              <li><img src={icone}></img></li>
-            )) }
-        </ul>
-      </header>
+    return (
+        <div className='App'>
+      <Header />
     </div>
   );
 }
 
-export default App;
+export default App
