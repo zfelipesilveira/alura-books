8fecb53bd160eb4e93399a92508bd2cc4b85ee45
Zomer Silveira <zfelipesilveira@gmail.com>
2023-10-20 14:52:23 -0300

    implementando pesquisa e dados do app

--- src/componentes/logo/index.js
+++ src/componentes/logo/index.js
@@ -1,12 +1,23 @@
 import logo from '../../imagens/logo.svg'
-import './estilo.css'
+import styled from 'styled-components'
+
+const LogoContainer = styled.div`
+    display: flex;
+    font-size: 30px;
+`
+const LogoImage = styled.img`
+    margin-right: 10px;
+`
 
 function Logo () {
     return(
-        <div className='logo'>
-            <img src={logo} alt='logo' className="logo-img"></img>
+        <LogoContainer>
+            <LogoImage
+                src={logo}
+                alt='logo'
+            />
             <p><strong>Alura</strong>Books</p>
-        </div>
+        </LogoContainer>
     )
 }
 
